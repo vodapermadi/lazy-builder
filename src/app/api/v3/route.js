e55f4db409e8812cc96365d8f9aa9eb0490d330b
vodapermadi <yapsitusaya111@gmail.com>
2024-06-01 08:01:57 +0700

    add cron job

--- src/app/api/v3/route.js
+++ src/app/api/v3/route.js
@@ -4,13 +4,13 @@ import { NextResponse } from "next/server"
 export const POST = async(req) => {
     try {
         const body = await req.formData()
-        console.log(body)
-        // const { data } = await axios.post(`${process.env.BACKEND_ENDPOINT}/upload_multiple`,body,{
-        //     headers:{
-        //         "Content-Type":"multipart/form-data"
-        //     }
-        // })
-        return NextResponse.json(body)
+        // console.log(body)
+        const { data } = await axios.post(`${process.env.BACKEND_ENDPOINT}/upload_multiple`,body,{
+            headers:{
+                "Content-Type":"multipart/form-data"
+            }
+        })
+        return NextResponse.json(data)
     } catch (error) {
         return NextResponse.json(error)
     }
